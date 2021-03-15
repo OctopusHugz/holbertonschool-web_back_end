cf9afe401642a46d035617cefe8c21a60fe4960a
OctopusHugz <colsonscott53@gmail.com>
2021-03-15 16:20:27 +0000

    Added 8-make_multiplier.py

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module implements a make_multiplier function"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Takes a float and returns a function that multiplies a float by the
+parameter multiplier"""
+    def new_func(new_multiplier: float) -> float:
+        """Takes a float and multiplies it by multiplier"""
+        return multiplier * new_multiplier
+    return new_func
