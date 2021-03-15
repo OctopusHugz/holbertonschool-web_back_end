0b832f3be970f40d4f0cf0cc949728550bf1a5b9
OctopusHugz <colsonscott53@gmail.com>
2021-03-15 15:37:46 +0000

    Added 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module implements a floor function"""
+import math
+
+
+def floor(n: float) -> int:
+    """This function takes a float and returns the floor"""
+    return math.floor(n)
