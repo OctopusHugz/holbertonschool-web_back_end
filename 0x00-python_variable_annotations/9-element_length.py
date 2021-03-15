553195e8b7dcaa63a2094042af708614bdd35e24
OctopusHugz <colsonscott53@gmail.com>
2021-03-15 16:24:16 +0000

    Added 9-element_length.py

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module works on duck types"""
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """This function returns the length of each element in a list as a tuple"""
+    return [(i, len(i)) for i in lst]
