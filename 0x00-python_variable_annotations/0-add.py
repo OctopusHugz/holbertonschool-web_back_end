9e62ebfef21b2d29e5a1387cd289cd71f22e1e07
OctopusHugz <colsonscott53@gmail.com>
2021-03-15 15:28:48 +0000

    Added 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module implements an add function"""
+
+
+def add(a: float, b: float) -> float:
+    """This function adds a and b and returns the result"""
+    return a + b
