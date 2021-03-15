ce6150a4805404b40ac03a2a6d8e8f4b6eb1f6d7
OctopusHugz <colsonscott53@gmail.com>
2021-03-15 16:58:47 +0000

    Added 101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module is working on duck typing"""
+from typing import Any, Mapping, TypeVar, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None]) -> Union[Any, T]:
+    """Safely fetches value from a dictionary if key exists in dct"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
