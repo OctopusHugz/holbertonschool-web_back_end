71ed1d6f6d03815036f2cd9eb128f4183682a509
OctopusHugz <colsonscott53@gmail.com>
2021-03-15 15:32:22 +0000

    Added 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module implements a concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function concats str1 and str2 and returns the result"""
+    return str1 + str2
