93a168e64a7923405cbc0a2e112e06baa74c5004
OctopusHugz <colsonscott53@gmail.com>
2021-03-16 17:22:20 +0000

    Added spaces around int=10

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """This async function awaits a random delay and returns its value"""
     random_delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
