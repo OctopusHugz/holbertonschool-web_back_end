e722632a7194742021369647bb6e72305e7eb79f
OctopusHugz <colsonscott53@gmail.com>
2021-03-16 19:26:17 +0000

    Updated logic for 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This module focuses on asyncio basics of concurrent coroutines"""
-from typing import List
+import asyncio
+from typing import Any, List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -8,7 +9,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns wait_random n times with specified max_delay"""
     delay_list: List[float] = []
-    for _ in range(0, n):
-        random_delay: int = await wait_random(max_delay)
-        delay_list.append(random_delay)
+    tasks: List[int] = []
+    for _ in range(n):
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+    for res in asyncio.as_completed(tasks):
+        completed = await res
+        delay_list.append(completed)
     return delay_list
