c037cd3d166c9aa362a8a1b866f2ae65b53a68c2
OctopusHugz <colsonscott53@gmail.com>
2021-03-16 19:27:32 +0000

    Fixed tasks annotation from int to Any

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns wait_random n times with specified max_delay"""
     delay_list: List[float] = []
-    tasks: List[int] = []
+    tasks: List[Any] = []
     for _ in range(n):
         tasks.append(asyncio.create_task(wait_random(max_delay)))
     for res in asyncio.as_completed(tasks):
