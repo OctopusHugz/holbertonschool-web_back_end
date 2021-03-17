c8b997bdfe67d00576c74233a0f9fb9446b24a5d
OctopusHugz <colsonscott53@gmail.com>
2021-03-17 17:14:41 +0000

    Changed annotation back, changed None to float

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float]:
+async def async_generator() -> AsyncGenerator[float, float]:
     """This function creates an async generator"""
     for _ in range(10):
         await asyncio.sleep(1)
