c51d6d805341a028f133024ab85c550997f6261c
OctopusHugz <colsonscott53@gmail.com>
2021-03-17 15:25:50 +0000

    Added 2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module handles the basics of async comprehension"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    start_time = time.time()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    end_time = time.time()
+    return (end_time - start_time)
