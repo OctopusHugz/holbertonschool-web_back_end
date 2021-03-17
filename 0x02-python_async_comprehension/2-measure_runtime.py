0cf2e71a3c6377264b31cd8959e7dbedd4d38b10
OctopusHugz <colsonscott53@gmail.com>
2021-03-17 15:37:17 +0000

    Added function documentation

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,6 +6,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime():
+    """This function measure the runtime of 4 async_comprehension functions"""
     start_time = time.time()
     await asyncio.gather(async_comprehension(), async_comprehension(),
                          async_comprehension(), async_comprehension())
