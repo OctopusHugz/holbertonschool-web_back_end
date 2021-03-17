ee004fe0178a786404998caaf72adbc52df1dfb8
OctopusHugz <colsonscott53@gmail.com>
2021-03-17 14:40:33 +0000

    Added 0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module handles the basics of async comprehension"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """This function creates an async generator"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
