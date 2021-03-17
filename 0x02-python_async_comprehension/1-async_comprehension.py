7850f0f978210a0c8f99049aaf3f64a1aac79e28
OctopusHugz <colsonscott53@gmail.com>
2021-03-17 17:06:58 +0000

    Added type annotation for return List[float]

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """This module handles the basics of async comprehension"""
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """This function creates an async generator"""
     return [i async for i in async_generator()]
