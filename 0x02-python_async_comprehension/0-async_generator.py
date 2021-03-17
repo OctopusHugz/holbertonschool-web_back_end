aa6e44e1c7883d3942ad64a0e7bcd9018cc624a2
OctopusHugz <colsonscott53@gmail.com>
2021-03-17 17:02:52 +0000

    Added type annotation for generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """This module handles the basics of async comprehension"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[int, None, None]:
     """This function creates an async generator"""
     for _ in range(10):
         await asyncio.sleep(1)
