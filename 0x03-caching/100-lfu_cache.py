65f97b5cd5b8c5d23303d68ca08ed7a00e1d0704
OctopusHugz <colsonscott53@gmail.com>
2021-03-21 16:33:06 +0000

    Shortened variable names

--- 0x03-caching/100-lfu_cache.py
+++ 0x03-caching/100-lfu_cache.py
@@ -6,55 +6,54 @@ from datetime import datetime
 
 class LFUCache(BaseCaching):
     """ LFU cache system """
-    timed_cache_data = {}
-    counted_cache_data = {}
-    lfks = {}
+    # timed_cache_data
+    tcd = {}
+    # counted_cache_data
+    ccd = {}
+    # least_frequent_keys
+    lfk = {}
 
     def put(self, key, item):
         """ Puts key and item in cache dictionary """
         if key is not None and item is not None:
             self.cache_data[key] = item
-            if self.counted_cache_data.get(key) is None:
-                self.counted_cache_data[key] = 1
+            if self.ccd.get(key) is None:
+                self.ccd[key] = 1
             else:
-                self.counted_cache_data[key] += 1
-            self.timed_cache_data[key] = datetime.now().strftime(
-                "%m/%d/%Y %H:%M:%S:%f")
+                self.ccd[key] += 1
+            self.tcd[key] = datetime.now().strftime("%m/%d/%Y %H:%M:%S:%f")
             if len(self.cache_data) > self.MAX_ITEMS:
-                lowest_count = min(self.counted_cache_data.values())
-                if lowest_count == 1 and self.counted_cache_data[key] == 1:
-                    lowest_list = sorted(list(
-                        self.counted_cache_data.values()))
+                lowest_count = min(self.ccd.values())
+                if lowest_count == 1 and self.ccd[key] == 1:
+                    lowest_list = sorted(list(self.ccd.values()))
                     lowest_list.pop(0)
                     lowest_count = lowest_list[0]
-                least_used_count = list(self.counted_cache_data.values()
-                                        ).count(lowest_count)
+                least_used_count = list(self.ccd.values()).count(lowest_count)
                 if least_used_count > 1:
-                    self.lfks = {}
-                    for keys, vals in self.counted_cache_data.copy().items():
+                    self.lfk = {}
+                    for keys, vals in self.ccd.copy().items():
                         if vals == lowest_count:
-                            self.lfks[keys] = self.timed_cache_data.get(keys)
-                    lfru_key = min(self.lfks)
+                            self.lfk[keys] = self.tcd.get(keys)
+                    lfru_key = min(self.lfk)
                     del self.cache_data[lfru_key]
-                    del self.timed_cache_data[lfru_key]
-                    del self.counted_cache_data[lfru_key]
+                    del self.tcd[lfru_key]
+                    del self.ccd[lfru_key]
                     print(f"DISCARD: {lfru_key}")
                 else:
-                    for k, v in self.counted_cache_data.copy().items():
+                    for k, v in self.ccd.copy().items():
                         if v == lowest_count:
                             del self.cache_data[k]
-                            del self.timed_cache_data[k]
-                            del self.counted_cache_data[k]
+                            del self.tcd[k]
+                            del self.ccd[k]
                             print(f"DISCARD: {k}")
 
     def get(self, key):
         """ Gets key from cache dictionary if it exists """
         value = self.cache_data.get(key)
         if value is not None:
-            if self.counted_cache_data.get(key) is None:
-                self.counted_cache_data[key] = 1
+            if self.ccd.get(key) is None:
+                self.ccd[key] = 1
             else:
-                self.counted_cache_data[key] += 1
-            self.timed_cache_data[key] = datetime.now().strftime(
-                "%m/%d/%Y %H:%M:%S:%f")
+                self.ccd[key] += 1
+            self.tcd[key] = datetime.now().strftime("%m/%d/%Y %H:%M:%S:%f")
         return value
