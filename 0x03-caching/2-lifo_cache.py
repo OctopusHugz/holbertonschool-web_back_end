b5b21ded4530a545e681fe187331586a0e25e59e
OctopusHugz <colsonscott53@gmail.com>
2021-03-21 16:20:02 +0000

    Removed commented out old version

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -6,21 +6,6 @@ from datetime import datetime
 
 class LIFOCache(BaseCaching):
     """ LIFO cache system """
-    # cache_list = []
-
-    # def put(self, key, item):
-    #     """ Puts key and item in cache dictionary """
-    #     if key is not None and item is not None:
-    #         self.cache_data[key] = item
-    #         if key in self.cache_list:
-    #             old_key_index = self.cache_list.index(key)
-    #             self.cache_list.pop(old_key_index)
-    #         self.cache_list.append(key)
-    #         if len(self.cache_data) > self.MAX_ITEMS:
-    #             popped_key = self.cache_list.pop(3)
-    #             print(f"DISCARD: {popped_key}")
-    #             del self.cache_data[popped_key]
-
     timed_cache_data = {}
 
     def put(self, key, item):
