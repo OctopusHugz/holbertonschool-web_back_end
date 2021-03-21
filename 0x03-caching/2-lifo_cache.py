b8e9492342764c3d173eac7ab49a63903b925966
OctopusHugz <colsonscott53@gmail.com>
2021-03-21 16:18:39 +0000

    Changed old LIFO to mru clone

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -1,24 +1,41 @@
 #!/usr/bin/env python3
 """This module implements a LIFO cache"""
 from base_caching import BaseCaching
+from datetime import datetime
 
 
 class LIFOCache(BaseCaching):
     """ LIFO cache system """
-    cache_list = []
+    # cache_list = []
+
+    # def put(self, key, item):
+    #     """ Puts key and item in cache dictionary """
+    #     if key is not None and item is not None:
+    #         self.cache_data[key] = item
+    #         if key in self.cache_list:
+    #             old_key_index = self.cache_list.index(key)
+    #             self.cache_list.pop(old_key_index)
+    #         self.cache_list.append(key)
+    #         if len(self.cache_data) > self.MAX_ITEMS:
+    #             popped_key = self.cache_list.pop(3)
+    #             print(f"DISCARD: {popped_key}")
+    #             del self.cache_data[popped_key]
+
+    timed_cache_data = {}
 
     def put(self, key, item):
         """ Puts key and item in cache dictionary """
         if key is not None and item is not None:
             self.cache_data[key] = item
-            if key in self.cache_list:
-                old_key_index = self.cache_list.index(key)
-                self.cache_list.pop(old_key_index)
-            self.cache_list.append(key)
+            self.timed_cache_data[key] = datetime.now().strftime(
+                "%m/%d/%Y %H:%M:%S:%f")
             if len(self.cache_data) > self.MAX_ITEMS:
-                popped_key = self.cache_list.pop(3)
-                print(f"DISCARD: {popped_key}")
-                del self.cache_data[popped_key]
+                most_recent = sorted(self.timed_cache_data.values())[-2]
+                for k, val in self.timed_cache_data.copy().items():
+                    if val == most_recent:
+                        del self.cache_data[k]
+                        del self.timed_cache_data[k]
+                        print(f"DISCARD: {k}")
 
     def get(self, key):
         """ Gets key from cache dictionary if it exists """
