80e470f9365bbf16cbaadf9ab3075ca3497bcd76
OctopusHugz <colsonscott53@gmail.com>
2021-03-25 22:15:40 +0000

    Added logic for exercise 0

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """ This module creates a filter_datum function """
 from typing import List
+import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ Returns the log message obfuscated """
+    return re.sub(f"(?<={fields}=).*?(?={separator})", redaction, message)
