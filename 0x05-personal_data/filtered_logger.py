0a474e1ef5ac927e1a636d63fcd125fb8e7002e7
OctopusHugz <colsonscott53@gmail.com>
2021-03-25 23:07:03 +0000

    Fixed logic for 0 using list comprehension one liner. Added outline for 1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,9 +2,26 @@
 """ This module creates a filter_datum function """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ Returns the log message obfuscated """
-    return re.sub(f"(?<={fields}=).*?(?={separator})", redaction, message)
+    return [message := re.sub(f"(?<={field}=).*?(?={separator})", redaction,
+                              message) for field in fields][-1]
