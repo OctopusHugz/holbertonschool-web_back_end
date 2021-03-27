94df6381a1962bb50510a4c9c9434866a1e9493d
OctopusHugz <colsonscott53@gmail.com>
2021-03-27 19:19:33 +0000

    Added logic for task 6

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """ This module handles encrypting passwords using bcrypt and hashpw """
-from bcrypt import gensalt, hashpw
+from bcrypt import gensalt, hashpw, checkpw
 
 
 def hash_password(password: str) -> bytes:
     """ Hashes a password given as argument using hashpw with a random salt"""
-    if not isinstance(password, bytes):
-        password = password.encode()
-    return hashpw(password, gensalt())
+    return hashpw(password.encode(), gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Checks whether a hashed password matches a non-hashed password """
+    return checkpw(password.encode(), hashed_password)
