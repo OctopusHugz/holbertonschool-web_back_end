52f7bba9494a22213bf0dad70f08709a3fed6ecd
OctopusHugz <colsonscott53@gmail.com>
2021-03-27 20:25:52 +0000

    Changed import to just import bcrypt instead of from bcrypt

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 """ This module handles encrypting passwords using bcrypt and hashpw """
-from bcrypt import checkpw, gensalt, hashpw
+import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """ Hashes a password given as argument using hashpw with a random salt"""
-    if isinstance(password, str):
-        return hashpw(password.encode(), gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Checks whether a hashed password matches a non-hashed password """
-    if isinstance(password, str) and isinstance(hashed_password, bytes):
-        return checkpw(password.encode(), hashed_password)
+    return bcrypt.checkpw(password.encode(), hashed_password)
