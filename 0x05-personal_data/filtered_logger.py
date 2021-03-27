059a1aedfaead1d7a814e742f840473509af19c6
OctopusHugz <colsonscott53@gmail.com>
2021-03-27 17:44:26 +0000

    Changed format to just return filter_datum

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -21,10 +21,9 @@ class RedactingFormatter(logging.Formatter):
         self.fields = list(fields)
 
     def format(self, record: logging.LogRecord) -> str:
-        record.asctime = self.formatTime(record)
-        record.message = filter_datum(
-            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
-        return self.formatMessage(record)
+        return filter_datum(
+            self.fields, self.REDACTION, super().format(record),
+            self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
