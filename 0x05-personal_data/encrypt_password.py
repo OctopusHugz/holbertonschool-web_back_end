bee938d02717c477ae8e929ae0483594089231f3
OctopusHugz <colsonscott53@gmail.com>
2021-03-27 19:52:19 +0000

    Trying workaround for maximum password length

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 """ This module handles encrypting passwords using bcrypt and hashpw """
 from bcrypt import gensalt, hashpw, checkpw
+import base64
+import hashlib
 
 
 def hash_password(password: str) -> bytes:
     """ Hashes a password given as argument using hashpw with a random salt"""
-    return hashpw(password.encode(), gensalt())
+    return hashpw(
+        base64.b64encode(hashlib.sha256(password.encode()).digest()), gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
