96113cae08b9b36c5d7e9c34b266da8f4351cdc6
OctopusHugz <colsonscott53@gmail.com>
2021-03-27 19:57:29 +0000

    Added logic for if password is None

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -5,9 +5,12 @@ from bcrypt import gensalt, hashpw, checkpw
 
 def hash_password(password: str) -> bytes:
     """ Hashes a password given as argument using hashpw with a random salt"""
-    return hashpw(password.encode(), gensalt())
+    if password:
+        return hashpw(password.encode(), gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Checks whether a hashed password matches a non-hashed password """
-    return checkpw(password.encode(), hashed_password)
+    if password:
+        return checkpw(password.encode(), hashed_password)
+    return False
