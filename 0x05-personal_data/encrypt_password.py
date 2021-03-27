12f1896a56ed478375d1447acd2f0e7796b7591f
OctopusHugz <colsonscott53@gmail.com>
2021-03-27 19:58:10 +0000

    Revert to previous version

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -5,12 +5,9 @@ from bcrypt import gensalt, hashpw, checkpw
 
 def hash_password(password: str) -> bytes:
     """ Hashes a password given as argument using hashpw with a random salt"""
-    if password:
-        return hashpw(password.encode(), gensalt())
+    return hashpw(password.encode(), gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Checks whether a hashed password matches a non-hashed password """
-    if password:
-        return checkpw(password.encode(), hashed_password)
-    return False
+    return checkpw(password.encode(), hashed_password)
