f6b943c45d0f6cc1f755ace16787c5016639f455
OctopusHugz <colsonscott53@gmail.com>
2021-03-27 19:01:40 +0000

    Added more to docstrings and fixed alphabetizing on imports

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" This module handles encrypting passwords """
-from typing import ByteString
+""" This module handles encrypting passwords using bcrypt and hashpw """
 from bcrypt import gensalt, hashpw
+from typing import ByteString
 
 
 def hash_password(password: str) -> ByteString:
-    """ Hashes a password given as argument """
+    """ Hashes a password given as argument using hashpw with a random salt"""
     return hashpw(password.encode(), gensalt())
