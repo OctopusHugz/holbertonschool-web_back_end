444e4a0a03fa0031e18fca585b538edd56fee605
OctopusHugz <colsonscott53@gmail.com>
2021-03-27 19:12:00 +0000

    Removed return type completely

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """ This module handles encrypting passwords using bcrypt and hashpw """
 from bcrypt import gensalt, hashpw
-# from typing import ByteString
 
 
-def hash_password(password: str) -> bytes:
+def hash_password(password: str):
     """ Hashes a password given as argument using hashpw with a random salt"""
     return hashpw(password.encode(), gensalt())
