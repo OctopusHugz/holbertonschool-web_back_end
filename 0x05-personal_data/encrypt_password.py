59c8523a187dd3f9b4b689cf4868df75645f0ea4
OctopusHugz <colsonscott53@gmail.com>
2021-03-27 20:22:50 +0000

    Try confirming parameter types

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -5,9 +5,11 @@ from bcrypt import checkpw, gensalt, hashpw
 
 def hash_password(password: str) -> bytes:
     """ Hashes a password given as argument using hashpw with a random salt"""
-    return hashpw(password.encode(), gensalt())
+    if isinstance(password, str):
+        return hashpw(password.encode(), gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Checks whether a hashed password matches a non-hashed password """
-    return checkpw(password.encode(), hashed_password)
+    if isinstance(password, str) and isinstance(hashed_password, bytes):
+        return checkpw(password.encode(), hashed_password)
