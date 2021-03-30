13d52d14cc15875501ebb454b9172d10700a0bd0
OctopusHugz <colsonscott53@gmail.com>
2021-03-30 16:22:35 +0000

    Added logic to app.py to create basic_auth depending on AUTH_TYPE. Added basic_auth.py

---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module implements the BasicAuth class """
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Instance of the BasicAuth class, which inherits from Auth """
