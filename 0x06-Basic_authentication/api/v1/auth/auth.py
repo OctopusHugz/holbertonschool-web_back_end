b5276c894c220a50bd6e1feada9f20b905394807
OctopusHugz <colsonscott53@gmail.com>
2021-03-30 15:33:01 +0000

    Updated require_auth method in auth.py

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,16 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Determines whether a given path requires auth """
-        return False
+        if path is None:
+            return True
+        elif excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ Returns the value of the header request Authorization """
