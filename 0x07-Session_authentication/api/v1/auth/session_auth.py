97882bb96f11ba5486707a51ca34fc4a4a2876f7
OctopusHugz <colsonscott53@gmail.com>
2021-04-05 21:54:20 +0000

    Added auth=SessionExpAuth() in app.py based on env var. Refactored current_user in session_auth.py to use user_id_for_session_id method. Added session_exp_auth.py to handle expiring sessions

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ This module implements the SessionAuth class """
-from flask.globals import session
 from api.v1.auth.auth import Auth
 from models.user import User
 import uuid
@@ -15,19 +14,19 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
-        SessionAuth.user_id_by_session_id[session_id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ Returns a User ID based on a Session ID """
         if session_id is None or not isinstance(session_id, str):
             return None
-        return SessionAuth.user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
         """ Returns a User instance based on a cookie value """
         session_id = self.session_cookie(request)
-        user_id = self.user_id_by_session_id.get(session_id)
+        user_id = self.user_id_for_session_id(session_id)
         return User.get(user_id)
 
     def destroy_session(self, request=None):
---
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" This module implements the SessionExpAuth class """
+from flask.globals import session
+from models.user import User
+from api.v1.auth.session_auth import SessionAuth
+from datetime import datetime, timedelta
+import os
+
+
+class SessionExpAuth(SessionAuth):
+    """Instance of the SessionExpAuth class, which inherits from SessionAuth"""
+    user_id_by_session_id = {}
+
+    def __init__(self):
+        """ Instantiate an instance of SessionExpAuth class """
+        sd = os.getenv("SESSION_DURATION")
+        if sd:
+            self.session_duration = int(sd)
+        else:
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """ Creates a session for a user_id """
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        session_dict = {}
+        session_dict["user_id"] = user_id
+        session_dict["created_at"] = datetime.now()
+        self.user_id_by_session_id[session_id] = session_dict
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """ Returns a user_id from a session_id """
+        if session_id is None or self.user_id_by_session_id.get(
+                session_id) is None:
+            return None
+        session_dict = self.user_id_by_session_id.get(session_id)
+        if self.session_duration <= 0:
+            return session_dict.get("user_id")
+        created_at = session_dict.get("created_at")
+        if created_at is None:
+            return None
+        expiration_date = created_at + timedelta(
+            seconds=self.session_duration)
+        if expiration_date < datetime.now():
+            return None
+        return session_dict.get("user_id")
+
+    # def current_user(self, request=None):
+    #     """ Returns a User instance based on a cookie value """
+    #     session_id = self.session_cookie(request)
+    #     user_id = self.user_id_for_session_id(session_id)
+    #     # print(f"session_id is: {session_id}")
+    #     # session_dict = self.user_id_by_session_id.get(session_id)
+    #     # print(f"session_dict is: {session_dict}")
+    #     # user_id = session_dict.get("user_id")
+    #     print(f"user_id is: {user_id}")
+    #     return User.get(user_id)
