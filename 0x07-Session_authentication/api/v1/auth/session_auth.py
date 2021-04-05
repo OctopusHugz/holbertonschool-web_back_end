233cbd28b779e08fa49fcf3950cff497f5c2cb25
OctopusHugz <colsonscott53@gmail.com>
2021-04-05 16:35:54 +0000

    Added outline files for implementing session_auth

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module implements the SessionAuth class """
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Instance of the SessionAuth class, which inherits from Auth """
