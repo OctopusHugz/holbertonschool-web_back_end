e73c71fc1bcda6c6da6f43187f561f6ff5716681
OctopusHugz <colsonscott53@gmail.com>
2021-04-06 18:30:33 +0000

    Updated logic to use filestorage

--- 0x07-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ This module implements the SessionDBAuth class """
+from models.base import DATA
 from models.user_session import UserSession
 from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 class SessionDBAuth(SessionExpAuth):
     """Instance of the SessionDBAuth class"""
-    user_id_by_session_id = {}
 
     def create_session(self, user_id=None):
         """ Creates a session for a user_id """
@@ -14,16 +14,22 @@ class SessionDBAuth(SessionExpAuth):
         if session_id is None:
             return None
         new_user_session = UserSession(session_id=session_id, user_id=user_id)
-        self.user_id_by_session_id[session_id] = new_user_session
+        new_user_session.save()
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """ Returns a user_id from a session_id """
-        user_session = self.user_id_by_session_id.get(session_id)
+        UserSession.load_from_file()
+        user_session = UserSession.search({"session_id": session_id})[0]
         if user_session is not None:
             return user_session.user_id
-        else:
-            return None
+        # UserSession.load_from_file()
+        # user_sessions = DATA["UserSession"]
+        # # could use UserSession.search here?!
+        # for user_session in user_sessions.values():
+        #     if user_session.session_id == session_id:
+        #         return user_session.user_id
+        return None
 
     def destroy_session(self, request=None):
         """ Deletes the user session/logs the user out """
@@ -32,7 +38,19 @@ class SessionDBAuth(SessionExpAuth):
         session_id = self.session_cookie(request)
         if session_id is None:
             return False
-        elif self.user_id_for_session_id(session_id) is None:
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
             return False
-        del self.user_id_by_session_id[session_id]
-        return True
+        # UserSession.load_from_file()
+        # user_sessions = DATA["UserSession"]
+        # for user_session in user_sessions.values():
+        #     if user_session.session_id == session_id\
+        #             and user_session.user_id == user_id:
+        #         user_session.remove()
+        # .remove() on UserSession
+        session_to_destroy = UserSession.search(
+            {"user_id": user_id, "session_id": session_id})
+        if session_to_destroy is not None:
+            session_to_destroy.remove()
+            return True
+        return False
