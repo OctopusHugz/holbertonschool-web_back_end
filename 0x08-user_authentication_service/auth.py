c10cd78ac7023bfeecbeea7c18bd0d8c786c83b9
OctopusHugz <colsonscott53@gmail.com>
2021-04-08 18:05:29 +0000

    Added return to self._db.add_user() to return User object

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -28,4 +28,4 @@ class Auth:
                 raise ValueError(f"User {email} already exists")
         except NoResultFound:
             hashed_password = _hash_password(password)
-            self._db.add_user(email, hashed_password)
+            return self._db.add_user(email, hashed_password)
