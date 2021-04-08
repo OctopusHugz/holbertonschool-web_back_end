da633369e991e019fcb4ae6c65e4bc9341547bdb
OctopusHugz <colsonscott53@gmail.com>
2021-04-08 18:10:29 +0000

    Added /users POST route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
 """ This module creates a Flask app """
-from flask import Flask, json, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
+# Do I really need to add methods=["GET"] in decorator?
 def hello():
+    """ Returns a jsonified message """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=["POST"])
+def users():
+    """ Implements the POST /users route """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        new_user = AUTH.register_user(email, password)
+        if new_user is not None:
+            return jsonify({"email": email,
+                            "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
