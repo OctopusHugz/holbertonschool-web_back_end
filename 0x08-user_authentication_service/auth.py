e1421834610c36c4e7c826cb7ed59662bff97b5b
OctopusHugz <colsonscott53@gmail.com>
2021-04-08 18:25:16 +0000

    Added valid_login method

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ This module implements an Auth class """
-from user import User
 from db import DB
 from sqlalchemy.exc import NoResultFound
 from typing import TypeVar
@@ -29,3 +28,13 @@ class Auth:
         except NoResultFound:
             hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Locates user by email, and checks password with bcrypt.checkpw"""
+        try:
+            found_user = self._db.find_user_by(email=email)
+            if found_user is not None:
+                return bcrypt.checkpw(password.encode(),
+                                      found_user.hashed_password.encode())
+        except NoResultFound:
+            return False
