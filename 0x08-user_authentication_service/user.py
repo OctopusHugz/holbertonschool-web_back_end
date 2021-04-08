ae90e9925dd4c538ea28382829fe59d1b30fc9ce
OctopusHugz <colsonscott53@gmail.com>
2021-04-08 15:51:25 +0000

    Added mapping declaration to user.py

---
+++ 0x08-user_authentication_service/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" This module creates a User class """
+from sqlalchemy import Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.sqltypes import INTEGER, VARCHAR
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(INTEGER, primary_key=True)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
+    session_id = Column(VARCHAR(250), nullable=True)
+    reset_token = Column(VARCHAR(250), nullable=True)
