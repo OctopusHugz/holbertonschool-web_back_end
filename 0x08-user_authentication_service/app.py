64f3cc7b45d88c4116892185027468c1ab404d3a
OctopusHugz <colsonscott53@gmail.com>
2021-04-09 00:01:16 +0000

    Added route /sessions with method DELETE and function logout()

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """ This module creates a Flask app """
 from flask.helpers import make_response
 from auth import Auth
-from flask import abort, Flask, jsonify, request
+from flask import abort, Flask, jsonify, redirect, request
 app = Flask(__name__)
 AUTH = Auth()
 
@@ -43,6 +43,27 @@ def login():
             return response
     else:
         abort(401)
+
+
+@app.route('/sessions', methods=["DELETE"])
+def logout():
+    """ Logs a user out of a session """
+    # The request is expected to contain the session ID
+    # as a cookie with key "session_id"
+    session_id = request.cookies.get("session_id")
+    if session_id is not None:
+        # Find the user with the requested session ID.
+        found_user = AUTH.get_user_from_session_id(session_id)
+        if found_user is not None:
+            # If the user exists destroy the session
+            AUTH.destroy_session(found_user.user_id)
+            # redirect to GET /
+            # do I need to add 302 code?
+            return redirect("/")
+
+        else:
+            # If the user does not exist, respond with a 403 HTTP status.
+            return jsonify({}, 403)
 
 
 if __name__ == "__main__":
