cecb2380684d91c01ec72166c5854d3e01450fa8
OctopusHugz <colsonscott53@gmail.com>
2021-04-09 23:47:28 +0000

    Changed self._engine

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -13,7 +13,9 @@ class DB:
 
     def __init__(self):
         # self._engine = create_engine("sqlite:///a.db", echo=True)
-        self._engine = create_engine("sqlite:///a.db")
+        self._engine = create_engine(
+            "sqlite:///a.db", connect_args={"check_same_thread": False})
+
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
