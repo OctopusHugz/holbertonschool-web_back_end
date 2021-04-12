0bab40d47fae79ddcf0814ba799be087b6e835f7
OctopusHugz <colsonscott53@gmail.com>
2021-04-12 03:24:47 +0000

    Trying an incorrect type annotation

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ This method adds a user to the DB """
         new_user = User(email=email, hashed_password=hashed_password)
         # new_user.email = email
