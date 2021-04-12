361370351f58d67adb8a1ac302d1ce428f6e680b
OctopusHugz <colsonscott53@gmail.com>
2021-04-12 03:15:42 +0000

    Rollback to echo=True

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -14,9 +14,9 @@ class DB:
 
     def __init__(self):
         """ Create DB instance """
-        # self._engine = create_engine("sqlite:///a.db", echo=True)
-        self._engine = create_engine(
-            "sqlite:///a.db", connect_args={"check_same_thread": False})
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        # self._engine = create_engine(
+        #     "sqlite:///a.db", connect_args={"check_same_thread": False})
 
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
