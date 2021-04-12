06e9de4bddbea5ac53cef11e703c7fe586f8b4dc
OctopusHugz <colsonscott53@gmail.com>
2021-04-12 05:27:28 +0000

    Checking parameters

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -98,12 +98,14 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """ Updates a user password to provided password using reset token """
-        try:
-            found_user = self._db.find_user_by(reset_token=reset_token)
-        except NoResultFound:
-            raise ValueError
-        hashed_password = _hash_password(password)
-        user_data = {"hashed_password": hashed_password,
-                     "reset_token": None}
-        self._db.update_user(found_user.id, **user_data)
+        if reset_token and isinstance(reset_token, str):
+            try:
+                found_user = self._db.find_user_by(reset_token=reset_token)
+            except NoResultFound:
+                raise ValueError
+        if password and isinstance(password, str):
+            hashed_password = _hash_password(password)
+            user_data = {"hashed_password": hashed_password,
+                         "reset_token": None}
+            self._db.update_user(found_user.id, **user_data)
         return None
