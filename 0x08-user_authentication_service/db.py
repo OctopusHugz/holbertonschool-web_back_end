4d965706126ee8995274d0cbbdb80c76c313ff61
OctopusHugz <colsonscott53@gmail.com>
2021-04-12 22:05:11 +0000

    Added docstrings back

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,14 +4,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import IntegrityError
 
 from user import Base, User
 
 
 class DB:
+    """DB class
+    """
 
     def __init__(self):
+        """Initialize a new DB instance
+        """
         self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -19,6 +22,8 @@ class DB:
 
     @property
     def _session(self):
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
