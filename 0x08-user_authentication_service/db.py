a9f85edde43eaa04b37ee26116e2e322ad937092
OctopusHugz <colsonscott53@gmail.com>
2021-04-12 03:51:11 +0000

    Revert to User return type

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ This module creates a DB class """
-from typing import TypeVar
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -22,14 +21,14 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ This method adds a user to the DB """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs) -> TypeVar("User"):
+    def find_user_by(self, **kwargs) -> User:
         """ Returns first row found in users table with args from kwargs """
         return self._session.query(User).filter_by(**kwargs).one()
 
