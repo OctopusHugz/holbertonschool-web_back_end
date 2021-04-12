5c5cbb14fe27c9c9f99c989416a21f31f119ccd4
OctopusHugz <colsonscott53@gmail.com>
2021-04-12 03:54:25 +0000

    Added check for if user was found to update_user()

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -35,9 +35,10 @@ class DB:
     def update_user(self, user_id: int, **kwargs) -> None:
         """ Updates a user row with args from kwargs in the DB """
         found_user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            if key not in found_user.__dict__.keys():
-                raise ValueError
-            setattr(found_user, key, value)
-        self._session.commit()
+        if found_user is not None:
+            for key, value in kwargs.items():
+                if key not in found_user.__dict__.keys():
+                    raise ValueError
+                setattr(found_user, key, value)
+            self._session.commit()
         return None
