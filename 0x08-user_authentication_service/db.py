d54090000c601a5c66f5eea6b70cefeb17aa5c98
OctopusHugz <colsonscott53@gmail.com>
2021-04-15 04:39:55 +0000

    Revert back to their copy, but removed echo=True

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,6 +4,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
 
 from user import Base, User
 
@@ -12,16 +13,16 @@ class DB:
     """DB class
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self):
+    def _session(self) -> Session:
         """Memoized session object
         """
         if self.__session is None:
