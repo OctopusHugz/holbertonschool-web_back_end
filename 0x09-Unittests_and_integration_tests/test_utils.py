1b533a7b016a4f8f123bad58ef657c6d387a252c
OctopusHugz <colsonscott53@gmail.com>
2021-04-16 18:25:14 +0000

    Added outline for test_utils.py

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+""" This module tests the utils.py file """
+from parameterized import parameterized
+from utils import access_nested_map
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Class for testing access_nested_map function """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test function for access_nested_map """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
