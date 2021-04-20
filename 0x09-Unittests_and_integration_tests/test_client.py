c34af02ef21bbbbe207c455e872b2c6bf1d73afb
OctopusHugz <colsonscott53@gmail.com>
2021-04-20 15:22:57 +0000

    Removed extraneous comments

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -65,19 +65,14 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def setUpClass(cls):
         """ setUp function to run before tests """
         cls.get_patcher = patch(
-            'requests.get', return_value=TEST_PAYLOAD, side_effect=cls.side_effect())
-        # cls.get_patcher = patch('requests.get')
+            'requests.get', return_value=TEST_PAYLOAD,
+            side_effect=cls.side_effect())
         cls.get_patcher.start()
-        # cls.get_patcher.side_effect = cls.side_effect()
-        # cls.get_patcher.side_effect = cls.side_effect(cls.org_payload)
 
     @classmethod
     def side_effect(cls):
         """ Checks url for validity """
         assert cls.get_patcher.side_effect == TEST_PAYLOAD
-        # use side_effect to make sure the mock of requests.get(url).json()
-        # returns the correct fixtures for the various
-        # values of url that you anticipate to receive
 
     # only mock code that sends external requests
     # def test_public_repos(self):
