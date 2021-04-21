7e4e8b789a27a70a1531db230c634992fd1ff171
OctopusHugz <colsonscott53@gmail.com>
2021-04-21 16:54:30 +0000

    Added files for task 7

--- 0x0A-i18n/7-app.py
+++ 0x0A-i18n/7-app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request
+from flask import Flask, g, render_template, request
 from flask_babel import Babel, gettext
+from pytz import UnknownTimeZoneError, timezone
 app = Flask(__name__)
 babel = Babel(app)
 users = {
@@ -22,16 +23,69 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template("7-index.html", home_title=gettext(u"home_title"),
-                           home_header=gettext(u"home_header"))
+    if g.user_id in users.keys():
+        logged_in = True
+    else:
+        logged_in = False
+    logged_in_as = gettext(u"logged_in_as", username=None)
+    if logged_in:
+        logged_in_as = gettext(
+            u"logged_in_as", username=g.user.get("name"))
+    return render_template("7-index.html", logged_in=logged_in,
+                           logged_in_as=logged_in_as, home_title=gettext(
+                               u"home_title"),
+                           home_header=gettext(u"home_header"),
+                           not_logged_in=gettext(u"not_logged_in"))
 
 
 @babel.localeselector
 def get_locale():
-    locale = request.args.get("locale")
-    if locale is not None and locale in Config.LANGUAGES:
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    url_locale = request.args.get("locale")
+    if url_locale is not None and url_locale in Config.LANGUAGES:
+        return url_locale
+    if g.user is not None:
+        user_locale = g.user.get("locale")
+        if user_locale is not None and user_locale in Config.LANGUAGES:
+            return user_locale
+    header_locale = request.headers.get("Accept-Language")
+    if header_locale is not None and header_locale in Config.LANGUAGES:
+        return header_locale
+    return Config.BABEL_DEFAULT_LOCALE
+
+
+def get_user(user_id):
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    user_id = request.args.get("login_as")
+    if user_id is not None:
+        user_id = int(user_id)
+    g.user_id = user_id
+    g.user = get_user(user_id)
+    g.locale = get_locale()
+    g.timezone = get_timezone()
+
+
+@babel.timezoneselector
+def get_timezone():
+    url_timezone = request.args.get("timezone")
+    if url_timezone is not None:
+        try:
+            timezone(url_timezone)
+            return url_timezone
+        except UnknownTimeZoneError:
+            pass
+    if g.user is not None:
+        user_timezone = g.user.get("timezone")
+        if user_timezone is not None:
+            try:
+                timezone(user_timezone)
+                return user_timezone
+            except UnknownTimeZoneError:
+                pass
+    return Config.BABEL_DEFAULT_TIMEZONE
 
 
 if __name__ == "__main__":
