115b76fa8910bf330364e5f169340456f2bacd29
OctopusHugz <colsonscott53@gmail.com>
2021-04-22 05:26:23 +0000

    Try removing u from gettext strings

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -20,8 +20,8 @@ app.config.from_object(Config)
 @app.route('/')
 def index() -> Text:
     """ Returns the index.html page """
-    return render_template("3-index.html", home_title=gettext(u"home_title"),
-                           home_header=gettext(u"home_header"))
+    return render_template("3-index.html", home_title=gettext("home_title"),
+                           home_header=gettext("home_header"))
 
 
 @babel.localeselector
