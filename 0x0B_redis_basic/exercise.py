c85e33caa8ea9c1d8d3af9b5847d63e5ab939a9d
OctopusHugz <colsonscott53@gmail.com>
2021-04-26 20:06:25 +0000

    Added get method to class

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ This module creates a Cache class and interacts with Redis """
-from typing import Union
+from typing import Callable, Union
 from redis import Redis
 from uuid import uuid4
 
 
-class Cache(object):
+class Cache():
     """ This is an instance of the Cache class """
 
     def __init__(self) -> None:
@@ -19,3 +19,13 @@ class Cache(object):
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, None]:
+        """ Gets the value of key from Redis, if it exists """
+        if fn is not None:
+            value = fn(self._redis.get(key))
+        else:
+            value = self._redis.get(key)
+        if value is None:
+            return "(nil)"
+        return value
