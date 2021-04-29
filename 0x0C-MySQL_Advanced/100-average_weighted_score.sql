9d2df523b9f3c682118e3ce378475ffb29b84729
Colson Scott <colsonscott53@gmail.com>
2021-04-29 15:25:40 +0000

    Changed logic to be more dynamic

--- 0x0C-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,7 +6,7 @@ CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
 	IN user_id VARCHAR(255)
 )
 BEGIN
-	SELECT (sum(score * weight) / 300) * 100 FROM corrections LEFT JOIN projects ON corrections.project_id = projects.id WHERE user_id = corrections.user_id INTO @weighted_score;
+	SELECT sum(score * weight) / sum(weight * 100) * 100 FROM corrections LEFT JOIN projects ON corrections.project_id = projects.id WHERE user_id = corrections.user_id INTO @weighted_score;
 	UPDATE users SET average_score = @weighted_score WHERE user_id = users.id;
 END $$
 
