3767bb8496cac4262a1b6de46b251be7731c3033
OctopusHugz <colsonscott53@gmail.com>
2021-05-03 16:17:46 +0000

    Added 10-update_topics.py

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module creates an update_topics function """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ Changes all topics of a school document based on the name """
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
