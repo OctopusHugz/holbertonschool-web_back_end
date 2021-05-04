01ea48d5ec59b9d52ff5a77a15cdbc19f390a121
OctopusHugz <colsonscott53@gmail.com>
2021-05-04 05:19:37 +0000

    Pycode fix

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -4,4 +4,5 @@
 
 def update_topics(mongo_collection, name, topics):
     """ Changes all topics of a school document based on the name """
-    mongo_collection.update({"name": name}, {"$set": {"topics": topics}}, multi=True)
+    mongo_collection.update(
+        {"name": name}, {"$set": {"topics": topics}}, multi=True)
