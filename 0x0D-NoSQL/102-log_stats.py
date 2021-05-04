79d9345675cedffc3b3823bfd96096c82d5bad20
OctopusHugz <colsonscott53@gmail.com>
2021-05-04 05:07:07 +0000

    Added if name == main

--- 0x0D-NoSQL/102-log_stats.py
+++ 0x0D-NoSQL/102-log_stats.py
@@ -2,35 +2,36 @@
 """ This module creates a script for interacting with log stats """
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.logs
-collection = db.nginx
-all_docs = collection.find()
-total_count = collection.count_documents({})
-get_count = collection.count_documents({"method": "GET"})
-post_count = collection.count_documents({"method": "POST"})
-put_count = collection.count_documents({"method": "PUT"})
-patch_count = collection.count_documents({"method": "PATCH"})
-delete_count = collection.count_documents({"method": "DELETE"})
-status_count = collection.count_documents({"path": "/status"})
-print(f"{total_count} logs\nMethods:")
-print(f"\tmethod GET: {get_count}")
-print(f"\tmethod POST: {post_count}")
-print(f"\tmethod PUT: {put_count}")
-print(f"\tmethod PATCH: {patch_count}")
-print(f"\tmethod DELETE: {delete_count}")
-print(f"{status_count} status check")
-print("IPs:")
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+    all_docs = collection.find()
+    total_count = collection.count_documents({})
+    get_count = collection.count_documents({"method": "GET"})
+    post_count = collection.count_documents({"method": "POST"})
+    put_count = collection.count_documents({"method": "PUT"})
+    patch_count = collection.count_documents({"method": "PATCH"})
+    delete_count = collection.count_documents({"method": "DELETE"})
+    status_count = collection.count_documents({"path": "/status"})
+    print(f"{total_count} logs\nMethods:")
+    print(f"\tmethod GET: {get_count}")
+    print(f"\tmethod POST: {post_count}")
+    print(f"\tmethod PUT: {put_count}")
+    print(f"\tmethod PATCH: {patch_count}")
+    print(f"\tmethod DELETE: {delete_count}")
+    print(f"{status_count} status check")
+    print("IPs:")
 
-pipeline = [
-    {"$group": {
-        "_id": "$ip",
-        "count": {"$sum": 1}}},
-    {"$sort": {"count": -1}},
-    {"$limit": 10}
-]
-ip_list = collection.aggregate(pipeline)
-for ip in ip_list:
-    ip_addr = ip.get("_id")
-    ip_count = ip.get("count")
-    print(f"\t{ip_addr}: {ip_count}")
+    pipeline = [
+        {"$group": {
+            "_id": "$ip",
+            "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    ip_list = collection.aggregate(pipeline)
+    for ip in ip_list:
+        ip_addr = ip.get("_id")
+        ip_count = ip.get("count")
+        print(f"\t{ip_addr}: {ip_count}")
