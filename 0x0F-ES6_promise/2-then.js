4addbdcb49719789447066ee911313096d9d7181
OctopusHugz <colsonscott53@gmail.com>
2021-05-10 17:13:58 +0000

    Added 2-then.js

---
+++ 0x0F-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+function handleResponseFromAPI(promise) {
+  promise.then(() => ({ status: 200, body: 'success' })).then(() => {
+    console.log('Got a response from the API');
+  }).catch(() => Error());
+}
+
+export default handleResponseFromAPI;
