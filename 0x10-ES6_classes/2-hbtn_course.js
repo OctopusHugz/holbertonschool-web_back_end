a62b114e13da862b94f1c63ccdfc3d92a3ebfb3e
OctopusHugz <colsonscott53@gmail.com>
2021-05-12 15:04:35 +0000

    Added type checks to both constructor and setter

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
 export default class HolbertonCourse {
 	constructor(name, length, students) {
+		if (typeof name !== 'string') throw TypeError('Name must be a string');
+		if (typeof length !== 'number') throw TypeError('Length must be a number');
+		if (!Array.isArray(students)) throw TypeError('Students must be an array');
 		this._name = name;
 		this._length = length;
 		this._students = students;
@@ -8,19 +11,22 @@ export default class HolbertonCourse {
 	get name() {
 		return this._name;
 	}
-	set name(name) {
-		if (typeof name === 'string') { this._name = name; } else throw TypeError('Name must be a string');
+	set name(newName) {
+		if (typeof newName !== 'string') throw TypeError('Name must be a string');
+		this._name = newName;
 	}
 	get length() {
 		return this._length;
 	}
-	set length(length) {
-		if (typeof length === 'number') { this._length = length; } else throw TypeError('Length must be a number');
+	set length(newLength) {
+		if (typeof newLength !== 'number') throw TypeError('Length must be a number');
+		this._length = newLength;
 	}
 	get students() {
+		if (!Array.isArray(students)) throw TypeError('Students must be an array');
 		return this._students;
 	}
-	set students(students) {
-		if (Array.isArray(students)) { this._students = students; }
+	set students(newStudents) {
+		this._students = newStudents;
 	}
 }
