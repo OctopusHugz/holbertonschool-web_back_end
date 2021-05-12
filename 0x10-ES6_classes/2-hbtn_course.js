096a32b854a9b1c556bc63738b754189504e4e46
OctopusHugz <colsonscott53@gmail.com>
2021-05-12 14:35:55 +0000

    Added 2-hbtn_course.js

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+export default class HolbertonCourse {
+	constructor(name, length, students) {
+		this._name = name;
+		this._length = length;
+		this._students = students;
+	}
+	get name() {
+		return this._name;
+	}
+	set name(name) {
+		if (typeof name === 'string') { this._name = name; } else throw TypeError('Name must be a string');
+	}
+	get length() {
+		return this._length;
+	}
+	set length(length) {
+		if (typeof length === 'number') { this._length = length; } else throw TypeError('Length must be a number');
+	}
+	get students() {
+		return this._students;
+	}
+	set students(students) {
+		if (Array.isArray(students)) { this._students = students; }
+	}
+}
