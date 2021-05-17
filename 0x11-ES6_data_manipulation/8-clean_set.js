7983ae972a589e80d614987f8450d4438abb5dac
OctopusHugz <colsonscott53@gmail.com>
2021-05-17 17:14:03 +0000

    Changed Array.from(set) to [...set]

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (startString === '') return '';
-  const setArray = Array.from(set)
+  const setArray = [...set]
     .map((item) => (item.startsWith(startString) ? item.slice(startString.length) : ''))
     .filter((item) => item !== '');
   return setArray.join('-');
