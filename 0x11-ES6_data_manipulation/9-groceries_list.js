20d678e4e9fa1160616f9ee91f90fd13b2cf5567
OctopusHugz <colsonscott53@gmail.com>
2021-05-17 15:04:19 +0000

    Added 9-groceries_list.js

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const newMap = new Map();
+  newMap.set('Apples', 10)
+    .set('Tomatoes', 10)
+    .set('Pasta', 10)
+    .set('Rice', 1)
+    .set('Banana', 5);
+  return newMap;
+}
