c45fa5a9e88ac3254c5df2a971b22bade5d02791
OctopusHugz <colsonscott53@gmail.com>
2021-05-17 15:36:59 +0000

    Fixed pasta value

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -2,7 +2,7 @@ export default function groceriesList() {
   const newMap = new Map();
   newMap.set('Apples', 10)
     .set('Tomatoes', 10)
-    .set('Pasta', 10)
+    .set('Pasta', 1)
     .set('Rice', 1)
     .set('Banana', 5);
   return newMap;
