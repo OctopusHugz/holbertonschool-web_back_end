69d3ea74f9cb15a81e9dc90915e8ed599fd3b17d
OctopusHugz <colsonscott53@gmail.com>
2021-05-18 13:42:55 +0000

    Added .set syntax

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,9 @@
 export default function createInt8TypedArray(length, position, number) {
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
-  if (position >= 0 && position < length) { int8View[position] = number; } else { throw Error('Position outside range'); }
+  if (position >= 0 && position < length) { int8View.set([number], position); } else { throw Error('Position outside range'); }
   return int8View;
+  // const dataView = new DataView(buffer);
+  // if (position < length) { dataView.setUint8(position, number); } else { throw Error('Position outside range'); }
+  // return dataView;
 }
