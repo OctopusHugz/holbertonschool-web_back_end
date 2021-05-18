e60a2f6a94ed65d54e257209db71943e13be3e75
OctopusHugz <colsonscott53@gmail.com>
2021-05-18 14:41:12 +0000

    Added check on item inside of map

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   if (startString === '' || typeof startString !== 'string') return '';
   const setArray = [...set]
-    .map((item) => (item.startsWith(startString) ? item.slice(startString.length) : ''))
+    .map((item) => (typeof item === 'string' && item.startsWith(startString) ? item.slice(startString.length) : ''))
     .filter((item) => item !== '');
   return setArray.join('-');
 }
