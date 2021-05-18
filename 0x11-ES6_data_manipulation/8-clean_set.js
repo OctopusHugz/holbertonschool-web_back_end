3372b69529270935d2b6b9ad311b57acfa02d51b
OctopusHugz <colsonscott53@gmail.com>
2021-05-18 14:39:39 +0000

    Added check on startString

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (startString === '' || typeof startString !== 'string') return '';
   const setArray = [...set]
     .map((item) => (item.startsWith(startString) ? item.slice(startString.length) : ''))
     .filter((item) => item !== '');
