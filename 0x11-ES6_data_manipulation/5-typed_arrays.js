665abb84d0c3bf9284b3a89163b196f85fbf3329
OctopusHugz <colsonscott53@gmail.com>
2021-05-18 14:36:59 +0000

    Revert back to dataView. Fixed parameter name from number to value

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,6 @@
-export default function createInt8TypedArray(length, position, number) {
+export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-  if (position >= 0 && position < length) { int8View.set([number], position); } else { throw Error('Position outside range'); }
-  return int8View;
-  // const dataView = new DataView(buffer);
-  // if (position < length) { dataView.setUint8(position, number); } else { throw Error('Position outside range'); }
-  // return dataView;
+  const dataView = new DataView(buffer);
+  if (position < length) { dataView.setUint8(position, value); } else { throw Error('Position outside range'); }
+  return dataView;
 }
