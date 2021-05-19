624f301e875c0eafb2169a00fc3fbc81b1a6713b
OctopusHugz <colsonscott53@gmail.com>
2021-05-19 21:54:43 +0000

    Added 5-http.js

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const { url } = req;
+  if (url === '/') { res.end('Hello Holberton School!'); }
+  if (url === '/students') {
+    await countStudents(process.argv[2]).then((value) => {
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${value.students.length}\n`);
+      res.write(`Number of students in CS: ${value.csStudents.length}. List: ${value.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${value.sweStudents.length}. List: ${value.sweStudents.join(', ')}`);
+      res.end();
+    }).catch((err) => res.end(err.message));
+  }
+});
+
+app.listen(1245);
+module.exports = app;
