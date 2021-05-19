4887751677013bae81eeccb801bcbea2e16ed305
OctopusHugz <colsonscott53@gmail.com>
2021-05-19 22:15:58 +0000

    Added statusCode and setHeader to res

--- 0x12-Node_JS_basic/6-http_express.js
+++ 0x12-Node_JS_basic/6-http_express.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
