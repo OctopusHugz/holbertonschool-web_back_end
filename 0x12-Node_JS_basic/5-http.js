993e51e92668cf438aef073e934c2fb9178ef4f3
OctopusHugz <colsonscott53@gmail.com>
2021-05-22 15:39:12 +0000

    Added Error: to beginning of error message

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -5,22 +5,24 @@ const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   const { url } = req;
+  let fileName;
   if (url === '/') { res.end('Hello Holberton School!'); }
   if (url === '/students') {
     try {
-      await countStudents(process.argv[2]).then((value) => {
-        res.write('This is the list of our students\n');
-        res.write(`Number of students: ${value.students.length}\n`);
-        res.write(`Number of students in CS: ${value.csStudents.length}. List: ${value.csStudents.join(', ')}\n`);
-        res.end(`Number of students in SWE: ${value.sweStudents.length}. List: ${value.sweStudents.join(', ')}`);
-      }).catch((err) => {
-        res.statusCode = 404;
-        res.end(err.message);
-      });
+      [fileName] = [process.argv[2]];
     } catch (error) {
       res.statusCode = 404;
-      res.end(error.message);
+      res.end(`Error: ${error.message}`);
     }
+    await countStudents(fileName).then((value) => {
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${value.students.length}\n`);
+      res.write(`Number of students in CS: ${value.csStudents.length}. List: ${value.csStudents.join(', ')}\n`);
+      res.end(`Number of students in SWE: ${value.sweStudents.length}. List: ${value.sweStudents.join(', ')}`);
+    }).catch((err) => {
+      res.statusCode = 404;
+      res.end(`Error: ${err.message}`);
+    });
   }
 });
 
