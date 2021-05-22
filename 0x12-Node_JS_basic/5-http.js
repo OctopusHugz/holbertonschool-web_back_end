a007b7cfa5b47fd97437bcad9b0681c9082d51b8
OctopusHugz <colsonscott53@gmail.com>
2021-05-22 16:01:45 +0000

    Removed async/await from createServer arrow func

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -4,21 +4,14 @@ const countStudents = require('./3-read_file_async');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   const { url } = req;
-  let fileName;
+  const fileName = process.argv[2];
   if (url === '/') { res.end('Hello Holberton School!'); }
   if (url === '/students') {
-    try {
-      [fileName] = [process.argv[2]];
-    } catch (error) {
-      res.statusCode = 404;
-      res.write('This is the list of our students\n');
-      res.end(`Error: ${error.message}`);
-    }
-    await countStudents(fileName).then((value) => {
+    countStudents(fileName).then((value) => {
       res.write('This is the list of our students\n');
       res.write(`Number of students: ${value.students.length}\n`);
       res.write(`Number of students in CS: ${value.csStudents.length}. List: ${value.csStudents.join(', ')}\n`);
