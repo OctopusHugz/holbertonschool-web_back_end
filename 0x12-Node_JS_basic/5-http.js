14db903ff27bd1f46b4e22ac3948643e002a53fa
OctopusHugz <colsonscott53@gmail.com>
2021-05-22 15:50:16 +0000

    Added this is list of our students to beginning of error

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -13,13 +13,21 @@ const app = http.createServer(async (req, res) => {
   if (url === '/students') {
     try {
       [fileName] = [process.argv[2]];
-    } catch (error) { res.status(404).send(`Error: ${error.message}`); }
+    } catch (error) {
+      res.statusCode = 404;
+      res.write('This is the list of our students\n');
+      res.end(`Error: ${error.message}`);
+    }
     await countStudents(fileName).then((value) => {
       res.write('This is the list of our students\n');
       res.write(`Number of students: ${value.students.length}\n`);
       res.write(`Number of students in CS: ${value.csStudents.length}. List: ${value.csStudents.join(', ')}\n`);
       res.end(`Number of students in SWE: ${value.sweStudents.length}. List: ${value.sweStudents.join(', ')}`);
-    }).catch((err) => { res.status(404).send(`Error: ${err.message}`); });
+    }).catch((err) => {
+      res.statusCode = 404;
+      res.write('This is the list of our students\n');
+      res.end(`Error: ${err.message}`);
+    });
   }
 });
 
