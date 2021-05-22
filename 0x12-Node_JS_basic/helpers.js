93863d016db5453e5cacc56fae80cdebdd51c6fa
OctopusHugz <colsonscott53@gmail.com>
2021-05-22 17:44:51 +0000

    Added more helpers

--- 0x12-Node_JS_basic/helpers.js
+++ 0x12-Node_JS_basic/helpers.js
@@ -1,5 +1,12 @@
+function writeIndex(res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+}
+
 function writeResponse(res, data, showTotal) {
   res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.write('This is the list of our students\n');
 
   if (showTotal) res.write(`Number of students: ${data.students.length}\n`);
@@ -7,4 +14,13 @@ function writeResponse(res, data, showTotal) {
   res.end(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
 }
 
+function writeError(res, err) {
+  res.statusCode = 404;
+  res.statusMessage = err.message;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`This is the list of our students\n${err.message}`);
+}
+
+module.exports = writeIndex;
 module.exports = writeResponse;
+module.exports = writeError;
