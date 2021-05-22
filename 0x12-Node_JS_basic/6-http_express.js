1d4e044281b42e312210d650f4e9dd0662081d69
OctopusHugz <colsonscott53@gmail.com>
2021-05-22 17:59:21 +0000

    Added refatored version

--- 0x12-Node_JS_basic/6-http_express.js
+++ 0x12-Node_JS_basic/6-http_express.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const helpers = require('./helpers');
+
+const { writeIndex } = helpers;
+
+const hostname = '127.0.0.1';
+const port = 1245;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Hello Holberton School!');
-});
+app.get('/', (req, res) => writeIndex(res));
 
-app.listen(1245);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 module.exports = app;
