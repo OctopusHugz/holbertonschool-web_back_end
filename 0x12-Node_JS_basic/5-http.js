7899d74310b6a7ec529fcb446af6d089daf2384c
OctopusHugz <colsonscott53@gmail.com>
2021-05-22 15:44:58 +0000

    Added different syntax for error codes. Added server listening console.log

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const hostname = '127.0.0.1';
+const port = 1245;
+
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -10,21 +13,17 @@ const app = http.createServer(async (req, res) => {
   if (url === '/students') {
     try {
       [fileName] = [process.argv[2]];
-    } catch (error) {
-      res.statusCode = 404;
-      res.end(`Error: ${error.message}`);
-    }
+    } catch (error) { res.status(404).send(`Error: ${error.message}`); }
     await countStudents(fileName).then((value) => {
       res.write('This is the list of our students\n');
       res.write(`Number of students: ${value.students.length}\n`);
       res.write(`Number of students in CS: ${value.csStudents.length}. List: ${value.csStudents.join(', ')}\n`);
       res.end(`Number of students in SWE: ${value.sweStudents.length}. List: ${value.sweStudents.join(', ')}`);
-    }).catch((err) => {
-      res.statusCode = 404;
-      res.end(`Error: ${err.message}`);
-    });
+    }).catch((err) => { res.status(404).send(`Error: ${err.message}`); });
   }
 });
 
-app.listen(1245);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 module.exports = app;
