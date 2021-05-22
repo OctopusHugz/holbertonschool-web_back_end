e24bdf2e10e6e51f62020a88426352359e77ca30
OctopusHugz <colsonscott53@gmail.com>
2021-05-22 18:02:42 +0000

    Added server running line

--- 0x12-Node_JS_basic/full_server/server.js
+++ 0x12-Node_JS_basic/full_server/server.js
@@ -3,7 +3,12 @@ import express from 'express';
 const app = express();
 const indexRouter = require('./routes/index');
 
-app.listen(1245);
+const hostname = '127.0.0.1';
+const port = 1245;
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 app.use('/', indexRouter);
 app.use('/students', indexRouter);
 app.use('/students/:major', indexRouter);
