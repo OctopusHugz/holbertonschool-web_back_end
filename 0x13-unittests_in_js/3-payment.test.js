c29d242b3757729b409bffd60f9762b97ba5af84
OctopusHugz <colsonscott53@gmail.com>
2021-05-25 18:22:05 +0000

    Removed hooks and fixed expect require line

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -1,24 +1,16 @@
-const {
-	expect
-} = require("chai");
+const { expect } = require("chai");
 const sinon = require("sinon");
 
 const sendPaymentRequestToApi = require("./3-payment");
 
 describe('sendPaymentRequestToApi', () => {
-	let consoleSpy;
-
-	beforeEach(() => {
-		consoleSpy = sinon.spy(console, 'log')
-	})
-
-	afterEach(() => {
-		consoleSpy.restore()
-	})
+	let consoleSpy = sinon.spy(console, 'log')
 
 	it('validates usage of Utils.calculateNumber', () => {
 		sendPaymentRequestToApi(100, 20)
 
 		expect(consoleSpy.calledWith('The total is: 120')).to.be.true
 	})
+
+	consoleSpy.restore()
 })
