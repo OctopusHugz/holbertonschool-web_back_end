054282d08b8dcb1b453fdaa1070171b1b9d220f8
OctopusHugz <colsonscott53@gmail.com>
2021-05-25 18:50:32 +0000

    Added 4-payment.test.js

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -1,16 +1,22 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
 
+const Utils = require('./utils')
 const sendPaymentRequestToApi = require("./3-payment");
 
-describe('sendPaymentRequestToApi', () => {
-	let consoleSpy = sinon.spy(console, 'log')
+describe('sendPaymentRequestToApi', function () {
+	it('validates usage of Utils.calculateNumber', function () {
+		const calcNumStub = sinon.stub(Utils, 'calculateNumber').returns(10)
+		const consoleSpy = sinon.spy(console, 'log')
 
-	it('validates usage of Utils.calculateNumber', () => {
 		sendPaymentRequestToApi(100, 20)
 
-		expect(consoleSpy.calledWith('The total is: 120')).to.be.true
+		expect(calcNumStub.calledWith('SUM', 100, 20)).to.be.true
+		expect(calcNumStub.alwaysReturned(10)).to.be.true
+		expect(consoleSpy.calledWith('The total is: 10')).to.be.true
+
+		calcNumStub.restore()
+		consoleSpy.restore()
 	})
 
-	consoleSpy.restore()
 })
