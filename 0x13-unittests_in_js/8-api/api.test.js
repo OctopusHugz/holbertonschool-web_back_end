aceb4b2c1ff2ef39e1f0b9fef01099baaa03a655
OctopusHugz <colsonscott53@gmail.com>
2021-05-25 21:35:39 +0000

    Added done to request callback

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -2,12 +2,10 @@ const { expect } = require("chai");
 const request = require('request')
 
 describe('Index page', () => {
-	it('checks output of curling server for index page', () => {
-		// spy on console for API available message?
+	it('checks output of curling server for index page', (done) => {
 		request('http://localhost:7865', (err, res, body) => {
 			expect(res.statusCode).to.equal(200)
-			expect(res.body).to.equal('Welcome to the payment system')
-			expect(res.request.uri.port).to.equal('7865')
-		})
+			expect(res.body).to.equal('Welcome to the asdfds system')
+		}, done())
 	})
 })
