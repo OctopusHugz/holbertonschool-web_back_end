9b4eac4cb75ea58dc49ea965420b7575cd032286
OctopusHugz <colsonscott53@gmail.com>
2021-05-25 18:19:54 +0000

    Added outline files for task 4

---
+++ 0x13-unittests_in_js/4-payment.js
@@ -0,0 +1,9 @@
+const Utils = require('./utils')
+const calculateNumber = Utils.calculateNumber
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+	const finalTotal = calculateNumber('SUM', totalAmount, totalShipping)
+	console.log(`The total is: ${finalTotal}`)
+}
+
+module.exports = sendPaymentRequestToApi;
