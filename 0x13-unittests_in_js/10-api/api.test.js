30d99103e10e917398b97ed125513c696a221e1d
OctopusHugz <colsonscott53@gmail.com>
2021-05-25 21:53:21 +0000

    Added outline files for task 10

---
+++ 0x13-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,19 @@
+const { expect } = require("chai");
+const request = require('request')
+
+describe('Index page', () => {
+	it('checks output of curling server for index page with valid cart number', (done) => {
+		request('http://localhost:7865/cart/12', (error, response, body) => {
+			expect(response.statusCode).to.equal(200)
+			expect(response.body).to.equal('Payment methods for cart 12\n')
+		}, done())
+	})
+	it('checks output of curling server for index page with invalid cart number', (done) => {
+		request('http://localhost:7865/cart/hello', (error, response, body) => {
+			if (response) {
+				expect(response.statusCode).to.equal(404)
+				expect(response.body).to.equal('')
+			}
+		}, done())
+	})
+})
