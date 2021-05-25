ddf58b74f1ca57d8cd4fb1f04d7bd406d2271c66
OctopusHugz <colsonscott53@gmail.com>
2021-05-25 18:42:59 +0000

    Consolidated everything into it block

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -4,13 +4,11 @@ const sinon = require("sinon");
 const sendPaymentRequestToApi = require("./3-payment");
 
 describe('sendPaymentRequestToApi', () => {
-	let consoleSpy = sinon.spy(console, 'log')
-
 	it('validates usage of Utils.calculateNumber', () => {
+		let consoleSpy = sinon.spy(console, 'log')
 		sendPaymentRequestToApi(100, 20)
 
 		expect(consoleSpy.calledWith('The total is: 120')).to.be.true
+		consoleSpy.restore()
 	})
-
-	consoleSpy.restore()
 })
