3ab7bfb2f0a669700259f20abb6db136cb600d4a
OctopusHugz <colsonscott53@gmail.com>
2021-05-25 21:51:34 +0000

    Updated api.test.js

--- 0x13-unittests_in_js/9-api/api.test.js
+++ 0x13-unittests_in_js/9-api/api.test.js
@@ -2,12 +2,18 @@ const { expect } = require("chai");
 const request = require('request')
 
 describe('Index page', () => {
-	it('checks output of curling server for index page', () => {
-		// spy on console for API available message?
-		request('http://localhost:7865', (err, res, body) => {
-			expect(res.statusCode).to.equal(200)
-			expect(res.body).to.equal('Welcome to the payment system')
-			expect(res.request.uri.port).to.equal('7865')
-		})
+	it('checks output of curling server for index page with valid cart number', (done) => {
+		request('http://localhost:7865/cart/12', (error, response, body) => {
+			expect(response.statusCode).to.equal(200)
+			expect(response.body).to.equal('Payment methods for cart 12\n')
+		}, done())
+	})
+	it('checks output of curling server for index page with invalid cart number', (done) => {
+		request('http://localhost:7865/cart/hello', (error, response, body) => {
+			if (response) {
+				expect(response.statusCode).to.equal(404)
+				expect(response.body).to.equal('')
+			}
+		}, done())
 	})
 })
