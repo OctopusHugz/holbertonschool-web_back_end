cd6c5847510fc844c6c3f2ab1c4c4c1b3483d790
OctopusHugz <colsonscott53@gmail.com>
2021-05-25 17:10:56 +0000

    Fixed if curly brace on same line

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,6 +1,5 @@
 export default function calculateNumber(type, a, b) {
-	if (['SUM', 'SUBTRACT', 'DIVIDE'].indexOf(type) !== -1)
-	{
+	if (['SUM', 'SUBTRACT', 'DIVIDE'].indexOf(type) !== -1) {
 		if (type === 'SUM') return Math.round(a) + Math.round(b)
 		else if (type === 'SUBTRACT') return Math.round(a) - Math.round(b)
 		if (Math.round(b) === 0) return 'Error'
