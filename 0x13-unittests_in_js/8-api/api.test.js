12e13727ea99fdfbea1dc04312db574258e5d75f
OctopusHugz <colsonscott53@gmail.com>
2021-05-25 21:45:14 +0000

    Fixed body equal string back to correct

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -5,7 +5,7 @@ describe('Index page', () => {
 	it('checks output of curling server for index page', (done) => {
 		request('http://localhost:7865', (err, res, body) => {
 			expect(res.statusCode).to.equal(200)
-			expect(res.body).to.equal('Welcome to the asdfds system')
+			expect(res.body).to.equal('Welcome to the payment system')
 		}, done())
 	})
 })
