e5f4098fbe3b9a882468798c01c69238af9dcdf7
OctopusHugz <colsonscott53@gmail.com>
2021-05-26 05:44:27 +0000

    Added describes with different routes. Changed response.body to body

--- 0x13-unittests_in_js/9-api/api.test.js
+++ 0x13-unittests_in_js/9-api/api.test.js
@@ -2,18 +2,38 @@ const { expect } = require("chai");
 const request = require('request')
 
 describe('Index page', () => {
-	it('checks output of curling server for index page with valid cart number', (done) => {
-		request('http://localhost:7865/cart/12', (error, response, body) => {
-			expect(response.statusCode).to.equal(200)
-			expect(body).to.equal('Payment methods for cart 12')
-		}, done())
+	describe('GET /', () => {
+		it('checks output of GET /', (done) => {
+			request('http://localhost:7865/', (error, response, body) => {
+				expect(response.statusCode).to.equal(200)
+				expect(body).to.equal('Welcome to the payment system')
+			}, done())
+		})
 	})
-	it('checks output of curling server for index page with invalid cart number', (done) => {
-		request('http://localhost:7865/cart/hello', (error, response, body) => {
-			if (response) {
-				expect(response.statusCode).to.equal(404)
-				expect(response.body).to.equal('')
-			}
-		}, done())
+
+	describe('GET /cart/:id', () => {
+		it('checks output of curling server for index page with valid cart number', (done) => {
+			request('http://localhost:7865/cart/12', (error, response, body) => {
+				expect(response.statusCode).to.equal(200)
+				expect(body).to.equal('Payment methods for cart 12')
+			}, done())
+		})
+	})
+
+	describe('GET /cart/:id', () => {
+		it('checks output of curling server for index page with invalid cart number', (done) => {
+			request('http://localhost:7865/cart/hello', (error, response, body) => {
+				if (response) {
+					expect(response.statusCode).to.equal(404)
+					expect(body).to.equal('')
+				}
+			}, done())
+		})
 	})
 })
+
+
+
+
+
+
