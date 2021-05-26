686ecd09b2cc915451d98be7c407f17d036b25c2
OctopusHugz <colsonscott53@gmail.com>
2021-05-26 06:00:17 +0000

    Added more specific it descriptions with route repeat

--- 0x13-unittests_in_js/9-api/api.test.js
+++ 0x13-unittests_in_js/9-api/api.test.js
@@ -12,7 +12,7 @@ describe('Index page', () => {
 	})
 
 	describe('GET /cart/:id', () => {
-		it('checks output of curling server for index page with valid cart number', (done) => {
+		it('checks output of GET /cart/:id', (done) => {
 			request('http://localhost:7865/cart/12', (error, response, body) => {
 				expect(response.statusCode).to.equal(200)
 				expect(body).to.equal('Payment methods for cart 12')
@@ -20,8 +20,8 @@ describe('Index page', () => {
 		})
 	})
 
-	describe('GET /cart/:id with id = hello', () => {
-		it('checks output of curling server for index page with invalid cart number', (done) => {
+	describe('GET /cart/:id with id hello', () => {
+		it('checks output of GET /cart/hello', (done) => {
 			request('http://localhost:7865/cart/hello', (error, response, body) => {
 				if (response) {
 					expect(response.statusCode).to.equal(404)
@@ -31,9 +31,3 @@ describe('Index page', () => {
 		})
 	})
 })
-
-
-
-
-
-
