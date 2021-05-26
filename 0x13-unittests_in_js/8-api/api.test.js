3a4c968e508cccb7269be6f9ff3dca0dcfc2c8f7
OctopusHugz <colsonscott53@gmail.com>
2021-05-26 05:34:54 +0000

    Added extra describe with GET / as string

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -2,10 +2,12 @@ const { expect } = require("chai");
 const request = require('request')
 
 describe('Index page', () => {
-	it('checks output of curling server for index page', (done) => {
-		request('http://localhost:7865', (err, res, body) => {
-			expect(res.statusCode).to.equal(200)
-			expect(res.body).to.equal('Welcome to the payment system')
-		}, done())
+	describe('GET /', () => {
+		it('checks output of curling server for index page', (done) => {
+			request('http://localhost:7865', (err, res, body) => {
+				expect(res.statusCode).to.equal(200)
+				expect(res.body).to.equal('Welcome to the payment system')
+			}, done())
+		})
 	})
 })
