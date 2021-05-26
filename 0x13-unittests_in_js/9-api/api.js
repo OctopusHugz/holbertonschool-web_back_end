6449d3a4c910358914898f6f9d64aa3a45b70eb1
OctopusHugz <colsonscott53@gmail.com>
2021-05-26 06:06:28 +0000

    Updated /cart/:id route with number verification in route def

--- 0x13-unittests_in_js/9-api/api.js
+++ 0x13-unittests_in_js/9-api/api.js
@@ -5,8 +5,4 @@ const port = 7865
 
 app.listen(port, console.log(`API available on localhost port ${port}`))
 app.get('/', (req, res) => res.end('Welcome to the payment system'))
-app.get('/cart/:id', (req, res) => {
-	const idArray = req.params.id.match(/(\d+)/);
-	if (idArray) res.end(`Payment methods for cart ${idArray[0]}`)
-	else res.status(404).end()
-})
+app.get('/cart/:id([0-9]*)', (req, res) => res.end(`Payment methods for cart ${req.params.id}`))
