c1f123485230285ff79034955d3c5915dd36548f
OctopusHugz <colsonscott53@gmail.com>
2021-05-26 05:23:47 +0000

    Removed extraneous n ewline at end of res.end()

--- 0x13-unittests_in_js/9-api/api.js
+++ 0x13-unittests_in_js/9-api/api.js
@@ -7,6 +7,6 @@ app.listen(port, console.log(`API available on localhost port ${port}`))
 app.get('/', (req, res) => res.end('Welcome to the payment system'))
 app.get('/cart/:id', (req, res) => {
 	const idArray = req.params.id.match(/(\d+)/);
-	if (idArray) res.end(`Payment methods for cart ${idArray[0]}\n`)
+	if (idArray) res.end(`Payment methods for cart ${idArray[0]}`)
 	else res.status(404).end()
 })
