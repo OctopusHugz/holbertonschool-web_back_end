4c138683a8f4a6601bfc0dfb7df69edee5fb79ba
OctopusHugz <colsonscott53@gmail.com>
2021-05-27 17:49:12 +0000

    Added outlines for task 5 files

---
+++ 0x14-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,7 @@
+import redis from 'redis'
+
+const client = redis.createClient()
+
+client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`))
+client.on('connect', () => console.log('Redis client connected to the server'))
+
