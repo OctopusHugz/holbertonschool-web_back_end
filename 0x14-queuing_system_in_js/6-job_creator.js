a9ad3062738d4654b70818033254d22da97e5cfb
OctopusHugz <colsonscott53@gmail.com>
2021-05-27 18:41:01 +0000

    Added 6-job_creator.js

---
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,12 @@
+const kue = require('kue')
+const queue = kue.createQueue()
+const jobObj = {
+	phoneNumber: '123-456-7890',
+	message: 'Hey, it\'s me!',
+}
+const job = queue.create('push_notification_code', jobObj).save()
+
+job.on('enqueue', (id, type) => console.log(`Notification job created: ${job.id}`))
+	.on('complete', () => console.log('Notification job completed'))
+	.on('failed', () => console.log('Notification job failed'))
+	// or job.on('failed attempt')?
