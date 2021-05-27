c566f3f53c28cadca635028630d690ee868340a6
OctopusHugz <colsonscott53@gmail.com>
2021-05-27 17:58:37 +0000

    Added 5-subscriber.js

--- 0x14-queuing_system_in_js/5-subscriber.js
+++ 0x14-queuing_system_in_js/5-subscriber.js
@@ -4,3 +4,13 @@ const client = redis.createClient()
 
 client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`))
 client.on('connect', () => console.log('Redis client connected to the server'))
+
+client.subscribe('holberton school channel')
+
+client.on('message', (channel, message) => {
+	if (channel === 'holberton school channel') console.log(message)
+	if (message === 'KILL_SERVER') {
+		client.unsubscribe()
+		client.quit()
+	}
+})
