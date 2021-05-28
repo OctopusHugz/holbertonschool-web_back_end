024e5c6b789a8e69c880210448deff9e0fda2b08
OctopusHugz <colsonscott53@gmail.com>
2021-05-28 17:40:39 +0000

    Added outline for 100-seat.js

---
+++ 0x14-queuing_system_in_js/100-seat.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const redis = require('redis')
+const kue = require('kue')
+const { promisify } = require('util')
+
+const app = express()
+const client = redis.createClient()
+const queue = kue.createQueue()
+
+const promisifiedSet = promisify(client.set).bind(client)
+const asyncGet = promisify(client.get).bind(client)
+
+const port = 1245
+
+const reserveSeat = (number) => promisifiedSet('available_seats', number)
+const getCurrentAvailableSeats = async () => asyncGet('available_seats')
+
+client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`))
+client.on('connect', () => {
+	console.log('Redis client connected to the server')
+	reserveSeat(50)
+	let reservationEnabled = true
+})
+
+app.listen(port, console.log(`Stock app listening at http://localhost:${port}`))
+app.get('/available_seats', async (req, res) => {
+	const seats = await getCurrentAvailableSeats()
+	res.json({ numberOfAvailableSeats: seats })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Requirements:
+// Make sure to use promisify with Redis
+// Make sure to use the await/async keyword to get the value from Redis
+// Make sure the format returned by the web application is always JSON and not text
+// Make sure that only the allowed amount of seats can be reserved
+// Make sure that the main route is displaying the right number of seats
