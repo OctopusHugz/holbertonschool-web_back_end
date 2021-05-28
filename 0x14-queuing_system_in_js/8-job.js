9c7aa4d73cde085b529fbbf9b2215a4d9d48798d
OctopusHugz <colsonscott53@gmail.com>
2021-05-28 20:19:54 +0000

    Added test 8-job.test.js and updated logic on func in 8-job.js

--- 0x14-queuing_system_in_js/8-job.js
+++ 0x14-queuing_system_in_js/8-job.js
@@ -1,12 +1,16 @@
-export default function createPushNotificationsJobs(jobs, queue) {
+const createPushNotificationsJobs = (jobs, queue) => {
 	if (!Array.isArray(jobs)) throw new Error('Jobs is not an array')
 
 	jobs.forEach((obj) => {
-		const newJob = queue.create('push_notification_code_3', obj).save()
+		const newJob = queue.create('push_notification_code_3', obj)
 		newJob
-			.on('enqueue', () => console.log(`Notification job created: ${newJob.id}`))
 			.on('complete', () => console.log(`Notification job ${newJob.id} completed`))
 			.on('failed', (errorMessage) => console.log(`Notification job ${newJob.id} failed: ${errorMessage}`))
 			.on('progress', (progress, data) => console.log(`Notification job ${newJob.id} ${progress}% complete`))
+		newJob.save((err) => {
+			if (!err) console.log(`Notification job created: ${newJob.id}`)
+		})
 	})
 }
+
+module.exports = createPushNotificationsJobs;
