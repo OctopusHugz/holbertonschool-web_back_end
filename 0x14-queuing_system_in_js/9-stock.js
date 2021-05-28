fc5442482470fc3063cf5839d154b57f83938383
OctopusHugz <colsonscott53@gmail.com>
2021-05-28 03:26:39 +0000

    Added /list_products route

--- 0x14-queuing_system_in_js/9-stock.js
+++ 0x14-queuing_system_in_js/9-stock.js
@@ -1,18 +1,20 @@
 import express from 'express'
 
 const listProducts = [
-	{ id: 1, name: 'Suitcase 250', price: 50, stock: 4 },
-	{ id: 2, name: 'Suitcase 450', price: 100, stock: 10 },
-	{ id: 3, name: 'Suitcase 650', price: 350, stock: 2 },
-	{ id: 4, name: 'Suitcase 1050', price: 50, stock: 5 }
+	{ itemId: 1, name: 'Suitcase 250', price: 50, initialAvailableQuantity: 4 },
+	{ itemId: 2, name: 'Suitcase 450', price: 100, initialAvailableQuantity: 10 },
+	{ itemId: 3, name: 'Suitcase 650', price: 350, initialAvailableQuantity: 2 },
+	{ itemId: 4, name: 'Suitcase 1050', price: 50, initialAvailableQuantity: 5 }
 ]
 
 // The .filter() below will fail accessing [0] if no product in listProducts has that id
 const getItemById = (id) => listProducts.filter((product) => product.id === id)[0]
 
 const app = express()
-const PORT = 1245
-app.listen(PORT, console.log(`Server listening on localhost:${PORT}`))
+const port = 1245
+
+app.listen(port, console.log(`Stock app listening at http://localhost:${port}`))
+app.get('/list_products', (req, res) => res.json(listProducts))
 
 // Make sure to use promisify with Redis
 // Make sure to use the await/async keyword to get the value from Redis
