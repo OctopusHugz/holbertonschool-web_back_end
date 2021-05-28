5f118f6a5a4360dfb12ab2a87143bd9eccbc861b
OctopusHugz <colsonscott53@gmail.com>
2021-05-28 17:51:59 +0000

    Added /reserve_seat route and required logic

--- 0x14-queuing_system_in_js/100-seat.js
+++ 0x14-queuing_system_in_js/100-seat.js
@@ -10,16 +10,16 @@ const queue = kue.createQueue()
 const promisifiedSet = promisify(client.set).bind(client)
 const asyncGet = promisify(client.get).bind(client)
 
-const port = 1245
-
 const reserveSeat = (number) => promisifiedSet('available_seats', number)
 const getCurrentAvailableSeats = async () => asyncGet('available_seats')
+
+const port = 1245
+let reservationEnabled = true
 
 client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`))
 client.on('connect', () => {
 	console.log('Redis client connected to the server')
 	reserveSeat(50)
-	let reservationEnabled = true
 })
 
 app.listen(port, console.log(`Stock app listening at http://localhost:${port}`))
@@ -27,7 +27,16 @@ app.get('/available_seats', async (req, res) => {
 	const seats = await getCurrentAvailableSeats()
 	res.json({ numberOfAvailableSeats: seats })
 })
-
+app.get('/reserve_seat', (req, res) => {
+	if (!reservationEnabled) res.json({ status: 'Reservation are blocked' })
+	const newJob = queue.create('reserve_seat', {}).save((err) => {
+		if (err) res.json({ status: 'Reservation failed' })
+		res.json({ status: 'Reservation in process' })
+	})
+	newJob
+		.on('complete', () => console.log(`Seat reservation job ${newJob.id} completed`))
+		.on('failed', (errorMessage) => console.log(`Seat reservation job ${newJob.id} failed: ${errorMessage}`))
+})
 
 
 
