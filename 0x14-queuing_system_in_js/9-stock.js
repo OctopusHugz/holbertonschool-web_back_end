d35784fc62b68c45129e452c6f0726d1d302d621
OctopusHugz <colsonscott53@gmail.com>
2021-05-28 03:36:32 +0000

    Added redis client and stock functions

--- 0x14-queuing_system_in_js/9-stock.js
+++ 0x14-queuing_system_in_js/9-stock.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import redis from 'redis'
 
 const listProducts = [
 	{ itemId: 1, name: 'Suitcase 250', price: 50, initialAvailableQuantity: 4 },
@@ -10,11 +11,25 @@ const listProducts = [
 // The .filter() below will fail accessing [0] if no product in listProducts has that id
 const getItemById = (id) => listProducts.filter((product) => product.id === id)[0]
 
+// Create express app and define routes
 const app = express()
 const port = 1245
 
 app.listen(port, console.log(`Stock app listening at http://localhost:${port}`))
 app.get('/list_products', (req, res) => res.json(listProducts))
+
+
+// Connect to redis client
+const client = redis.createClient()
+
+client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`))
+client.on('connect', () => console.log('Redis client connected to the server'))
+
+const reserveStockById = (itemId, stock) => client.set(`item.${itemId}`, stock)
+const getCurrentReservedStockById = async (itemId) => {
+	const promisifiedFunc = promisify(client.get).bind(client)
+	return promisifiedFunc(schoolName)
+}
 
 // Make sure to use promisify with Redis
 // Make sure to use the await/async keyword to get the value from Redis
