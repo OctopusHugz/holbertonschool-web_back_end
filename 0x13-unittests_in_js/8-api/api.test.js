4d4813c26eeab080c8338c73da328877c89b5eec
OctopusHugz <colsonscott53@gmail.com>
2021-06-01 20:26:17 +0000

    Moved done() to inside request block

--- 0x13-unittests_in_js/8-api/api.test.js
+++ 0x13-unittests_in_js/8-api/api.test.js
@@ -7,7 +7,8 @@ describe('Index page', () => {
 			request('http://localhost:7865/', (err, res, body) => {
 				expect(res.statusCode).to.equal(200)
 				expect(body).to.equal('Welcome to the payment system')
-			}, done())
+				done()
+			})
 		})
 	})
 })
